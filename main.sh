1e5a73df87770d105e3e91acc3908c537a1d9b5b
fzjcdt <11510785@mail.sustech.edu.cn>
2023-08-01 20:29:39 +0800

    add main.sh

---
+++ main.sh
@@ -0,0 +1,3 @@
+python main_cifar10.py
+python main_cifar100.py
+python main_timagenet.py
--- main_cifar10.py
+++ main_cifar10.py
@@ -1,5 +1,6 @@
 from test_cifar10 import fgsm_uap_test
 
+
 fgsm_uap_test(seed=0)
 fgsm_uap_test(seed=1)
 fgsm_uap_test(seed=2)
