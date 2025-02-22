83728f5de10288510c16df703cb368bb7f70353b
fzjcdt <33198682+fzjcdt@users.noreply.github.com>
2025-02-22 11:16:59 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -8,7 +8,7 @@ python pgd_test_cifar100.py
 python aa_test_cifar100.py
 python cw_test_cifar100.py
 
-python main_cifar100.py
-python pgd_test_cifar100.py
-python aa_test_cifar100.py
-python cw_test_cifar100.py
+python main_tiny_imagenet.py
+python pgd_test_tiny_imagenet.py
+python aa_test_tiny_imagenet.py
+python cw_test_tiny_imagenet.py
