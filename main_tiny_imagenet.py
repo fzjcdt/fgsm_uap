9eef33894a396cb5fe6bc6ccbaffebb5d794a668
fzjcdt <33198682+fzjcdt@users.noreply.github.com>
2025-02-22 16:37:26 +0800

    Update main_tiny_imagenet.py

--- main_tiny_imagenet.py
+++ main_tiny_imagenet.py
@@ -1,6 +1,6 @@
 from test_tiny_imagenet import fgsm_uap_test
-from test_tiny_imagenet import fgsm_one_uap_test
-from test_tiny_imagenet import fgsm_class_uap_test
+# from test_tiny_imagenet import fgsm_one_uap_test
+# from test_tiny_imagenet import fgsm_class_uap_test
 
 fgsm_uap_test(seed=0)
 fgsm_uap_test(seed=1)
